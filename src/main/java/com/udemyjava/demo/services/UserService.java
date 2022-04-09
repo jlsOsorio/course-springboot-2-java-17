ec83f0f778e6b7e4f7ff0e8bcd29dd6b68adb661
Joao Osorio <joao_lso@hotmail.com>
2022-04-09 05:21:49 +0100

    Exception handling - update

--- src/main/java/com/udemyjava/demo/services/UserService.java
+++ src/main/java/com/udemyjava/demo/services/UserService.java
@@ -3,6 +3,8 @@ package com.udemyjava.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -49,9 +51,16 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id); //o getOne prepara o objecto pelo JPA (é monitorizado). Desta forma, não há necessidade de ir buscar o objecto à base de dados.
-		updateData(entity, obj);
-		return repository.save(entity);
+		try
+		{
+			User entity = repository.getOne(id); //o getOne prepara o objecto pelo JPA (é monitorizado). Desta forma, não há necessidade de ir buscar o objecto à base de dados.
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
--- src/main/java/com/udemyjava/demo/services/exceptions/DatabaseException.java
+++ src/main/java/com/udemyjava/demo/services/exceptions/DatabaseException.java
@@ -4,6 +4,7 @@ public class DatabaseException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
 	
+	//400 error - integridade
 	public DatabaseException(String msg) {
 		super(msg);
 	}
