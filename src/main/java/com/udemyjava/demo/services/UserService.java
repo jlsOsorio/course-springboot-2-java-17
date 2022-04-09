7054605b9cc9189e92d30768796874eb8a99306b
Joao Osorio <joao_lso@hotmail.com>
2022-04-09 05:14:11 +0100

    Exception handling - delete

--- src/main/java/com/udemyjava/demo/services/UserService.java
+++ src/main/java/com/udemyjava/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.udemyjava.demo.entities.User;
 import com.udemyjava.demo.repositories.UserRepository;
+import com.udemyjava.demo.services.exceptions.DatabaseException;
 import com.udemyjava.demo.services.exceptions.ResourceNotFoundException;
 
 @Service //regista a classe como componente do Spring para ele conhecer e ser automaticamente injectada (autowired). Existem também o Component e o Repository, para o mesmo fim
@@ -30,7 +33,19 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try
+		{
+			repository.deleteById(id);	
+		}
+		catch (EmptyResultDataAccessException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
