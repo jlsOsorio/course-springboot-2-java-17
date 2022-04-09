34800cbe648a0e9762c8d6a7f5b4e0b6700f88c8
Joao Osorio <joao_lso@hotmail.com>
2022-04-09 03:30:28 +0100

    Payment, one-to-one association

--- src/main/java/com/udemyjava/demo/config/TestConfig.java
+++ src/main/java/com/udemyjava/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemyjava.demo.entities.Category;
 import com.udemyjava.demo.entities.Order;
 import com.udemyjava.demo.entities.OrderItem;
+import com.udemyjava.demo.entities.Payment;
 import com.udemyjava.demo.entities.Product;
 import com.udemyjava.demo.entities.User;
 import com.udemyjava.demo.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
